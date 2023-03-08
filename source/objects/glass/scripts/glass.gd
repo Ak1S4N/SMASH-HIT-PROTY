a03a90b6a5ceffb05c16556b86746d747ea41b0e
Ak1S4N <107114197+Ak1S4N@users.noreply.github.com>
2023-03-08 16:36:50 +0800

    new

--- source/objects/glass/scripts/glass.gd
+++ source/objects/glass/scripts/glass.gd
@@ -1,14 +1,37 @@
 extends RigidBody
 
-onready var area := $Area
+#Context: THE GLASSSSS
+#Here, we tackle the physics and functions of the glass
+#Altough, this isn't like those glasses in the game that break...
+#This glass is enough to move and function like how you're hitting an object
 
+#We make a speed variable for when this node moves
+#The reason why we need to make the glass moves...
+#Due to functions that aren't present in Godot, we have to stick with this method
 var speed : float = 0.03
 
+
+#Now, if you know how to make something move smoothly, then this is an easy look
 func _physics_process(delta: float) -> void:
+	
+	#First, we take the origin and direction of our glass, we add the origin with speed
 	transform.origin.z += speed
+	
+	#We use the clamp function to limit the speed
 	clamp(transform.origin.z, -speed, speed)
 
+#We connect an Area node's (in the glass scene tree) signal to this script
 func _on_Area_body_entered(body: Node) -> void:
+	#This signal is used when a body (Rigid, Kinematic, Static, etc.) entered the area
+	#hence, the name
+	
+	#first, we identify the body that entered the area
 	if body.is_in_group("ball_bullet"):
+		
+		#next, if that body has been identified and found true,
+		#we set the mode of this glass from static to rigid
 		set_mode(RigidBody.MODE_RIGID)
+		
+		#lastly, we set the physics process (which handles the movement of the glass) to false
+		#So it would look like not moving and dead
 		set_physics_process(false)
--- source/worlds/scripts/world.gd
+++ source/worlds/scripts/world.gd
@@ -1,11 +1,23 @@
 extends Spatial
 
+#Context: THE WORLDDDDDD
+#Here, we make the 'world' move towards the player (just like in smash hit)
+#We would make an illusion that the camera is the one moving, but not really
 
+#FIRST, THE SPEED OF THE WORLD
 export var speed : float = 0.1
+
+#NEXT, we grab the wall nodes (check the scene tree for context) so we can control it
 onready var walls := $walls
 
+#Physics process... we already know what this is...
+#It makes a function active every fps
 func _physics_process(delta: float) -> void:
 	
+	#First, we get the origin and direction of the walls and give it speed
 	walls.transform.origin.z += speed
+	
+	#Next, we use the clamp function to limit the speed :>
+	#For more context of this function, press 'ctrl' + 'left mouse button' to the clamp function
 	clamp(walls.transform.origin.z, -3.0, 3.0)
 	
